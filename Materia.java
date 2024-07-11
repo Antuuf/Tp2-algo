298f98fd9f78a0cf9bf2b96ccf7a09a4de3261a5
Antuuf <98172181+Antuuf@users.noreply.github.com>
2024-07-11 18:10:36 -0300

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -1,77 +1,90 @@
 package aed;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Materia {
     private int[] docentes;
-    private ListaEnlazada<String> alumnos;
-    private ParCarreraMateria[] otrosNombres; //(nombredeltrie,nombremateria) //lista de los parCarrera
-    private ArrayList<Trie<Materia>> referencias; //guarda la referencia del trie para poder llamarlo despues y que no se vaya la complejidad
+    private List<String> alumnos;
+    private List<Trie<Materia>> referencias;
+    private List<String> otrosNombres;
 
-    public Materia(ParCarreraMateria[] info){
-        this.docentes= new int[4];
-        this.alumnos= new ListaEnlazada<>();
-        this.otrosNombres= info;
-        this.referencias= new ArrayList<Trie<Materia>>();
+    public Materia() {
+        this.docentes = new int[4];
+        this.alumnos = new ArrayList<>();
+        this.referencias = new ArrayList<>();
+        this.otrosNombres = new ArrayList<>();
     }
 
-    public void agregarReferencia(Trie<Materia> referencia){ 
+    public void agregarReferencia(Trie<Materia> referencia) {
         this.referencias.add(referencia);
     }
 
-    public ArrayList<Trie<Materia>> darReferencia(){
+    public List<Trie<Materia>> darReferencia() {
         return referencias;
     }
-    
-    public void agregarAlumno(String alumno){
-        this.alumnos.agregarAtras(alumno);
+
+    public void agregarAlumno(String alumno) {
+        this.alumnos.add(alumno);
     }
 
-    public void agregarDocente(String cargo){
-        if (cargo.equals("PROF")){
-            this.docentes[0]++;
-        }
-        if (cargo.equals("JTP")){
-            this.docentes[1]++;
-        }
-        if (cargo.equals("AY1")){
-            this.docentes[2]++;
-        }
-        if (cargo.equals("AY2")){
-            this.docentes[3]++;
+    public void agregarDocente(String cargo) {
+        switch (cargo) {
+            case "PROF":
+                this.docentes[0]++;
+                break;
+            case "JTP":
+                this.docentes[1]++;
+                break;
+            case "AY1":
+                this.docentes[2]++;
+                break;
+            case "AY2":
+                this.docentes[3]++;
+                break;
         }
     }
-    // Aca arreglé esta parte para que se agreguen bien los docentes
 
-    public int[] obtenerDocentes(){
+    public int[] obtenerDocentes() {
         return this.docentes;
     }
 
-    public ListaEnlazada<String> obtenerListaAlumnos(){
+    public List<String> obtenerListaAlumnos() {
         return alumnos;
     }
 
-    public ParCarreraMateria[] obtenerNombres(){
-        return otrosNombres;
-    }
-   
-    public int obtenerCantidadAlumnos(){
-        return this.alumnos.longitud();
+    public int obtenerCantidadAlumnos() {
+        return this.alumnos.size();
     }
 
     public boolean excedeCupo() {
         int cant = obtenerCantidadAlumnos();
-        return (docentes[0] * 250 < cant) || (docentes[1] * 100 < cant) || (docentes[2] * 20 < cant) || (docentes[3] * 30 < cant);
-    }  // no entendi la corrección que nos hicieron acá, porque esto funciona bien. Será que quiere que tomemos el mínimo de todos estos valores y sólo comparemos ese con la cantidad de alumnos, en lugar de comparar todos. Bueno en todo caso igual es facil de cambiar 
-   
+        int cupoProf = docentes[0] * 250;
+        int cupoJTP = docentes[1] * 100;
+        int cupoAY1 = docentes[2] * 20;
+        int cupoAY2 = docentes[3] * 30;
 
- // Método para obtener todas las materias equivalentes por nombre. Esto lo uso en CerrarMateria 
- public String[] obtenerNombresEquivalentes() {
-    String[] nombres = new String[otrosNombres.length];
-    for (int i = 0; i < otrosNombres.length; i++) {
-        nombres[i] = otrosNombres[i].getNombreMateria();
+        int cupoMinimo = cupoProf;
+        if (cupoJTP < cupoMinimo) {
+            cupoMinimo = cupoJTP;
+        }
+        if (cupoAY1 < cupoMinimo) {
+            cupoMinimo = cupoAY1;
+        }
+        if (cupoAY2 < cupoMinimo) {
+            cupoMinimo = cupoAY2;
+        }
+
+        return cant > cupoMinimo;
     }
-    return nombres;
-}
+
+    public List<String> obtenerNombresEquivalentes() {
+        return otrosNombres;
+    }
+
+    public void agregarNombreEquivalente(String nombre) {
+        if (!otrosNombres.contains(nombre)) {
+            otrosNombres.add(nombre);
+        }
+    }
 }
